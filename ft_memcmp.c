68d33cc46413da99cad7c6933ad0985acb489082
Manon Lemercier <mlemerci@paul-f4Br9s2.clusters.42paris.fr>
2025-02-22 19:18:06 +0100

    first

---
+++ ft_memcmp.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlemerci <mlemerci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/16 09:13:31 by mlemerci          #+#    #+#             */
+/*   Updated: 2024/11/29 21:35:58 by mlemerci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+	unsigned char	*str2;
+
+	i = 0;
+	str = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (i < n)
+	{
+		if (str[i] != str2[i])
+			return ((unsigned char)str[i] - (unsigned char)str2[i]);
+		i++;
+	}
+	return (0);
+}
+
+/*
+ int	main(void)
+ {
+		const void *s1 = "abcde";
+		const void *s2 = "bcde";
+		printf("%d\n", ft_memcmp(s1, s2, 3));
+ }*/
